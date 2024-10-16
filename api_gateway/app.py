cad4f299f7cdbb7603927e391434bbf69ba19837
Ivan Flitcroft <Ivanflitcroft@gmail.com>
2024-10-16 10:35:52 +0100

    Defined URLS for microservices (could do with putting in a .env)

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -3,5 +3,10 @@ import requests
 
 app = Flask(__name__)
 
+STUDENT_SERVICE_URL = "http://student_service:5000"
+TRAINER_SERVICE_URL = "http://trainer_service:5001"
+COURSE_SERVICE_URL = "http://course_service:5002"
+BOOKING_SERVICE_URL = "http://booking_service:5003"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
