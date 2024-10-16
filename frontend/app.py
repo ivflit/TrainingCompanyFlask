da5b89e57fcb9862fd51d6e9ffdb0d4a53bf2bf1
Ivan Flitcroft <Ivanflitcroft@gmail.com>
2024-10-16 12:33:12 +0100

    Added urls for the frontend, method to manage students

--- frontend/app.py
+++ frontend/app.py
@@ -3,9 +3,31 @@ import requests
 
 app = Flask(__name__)
 
+STUDENT_SERVICE_URL = "http://student_service:5003/students"
+TRAINER_SERVICE_URL = "http://trainer_service:5004/trainers"
+COURSE_SERVICE_URL = "http://course_service:5001/courses"
+BOOKING_SERVICE_URL = "http://booking_service:5002/students"
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/students', methods=['GET', 'POST'])
+def manage_students():
+    if request.method == 'POST':
+        data = {
+            'name': request.form['name'],
+            'age': request.form['age'],
+            'company': request.form['company'],
+            'level': request.form['level'],
+            'stream': request.form['stream']
+        }
+        response = requests.post(STUDENT_SERVICE_URL, json=data)
+        if response.status_code == 201:
+            return redirect(url_for('manage_students'))
+    
+    students = requests.get(STUDENT_SERVICE_URL).json()
+    return render_template('students.html', students=students)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
