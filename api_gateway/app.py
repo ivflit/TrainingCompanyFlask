7070f9f8369b5d8523913006eca94611c0f1cc93
Ivan Flitcroft <Ivanflitcroft@gmail.com>
2024-10-16 10:39:51 +0100

    POST, GET

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -8,6 +8,7 @@ TRAINER_SERVICE_URL = "http://trainer_service:5001"
 COURSE_SERVICE_URL = "http://course_service:5002"
 BOOKING_SERVICE_URL = "http://booking_service:5003"
 
+# STUDENTS
 @app.route('/students', methods=['POST'])
 def register_student():
     data = request.json
@@ -17,6 +18,18 @@ def register_student():
 @app.route('/students/<student_id>', methods=['GET'])
 def get_student(student_id):
     response = requests.get(f"{STUDENT_SERVICE_URL}/students/{student_id}")
+    return jsonify(response.json()), response.status_code
+
+# TRAINERS
+@app.route('/trainers', methods=['POST'])
+def register_trainer():
+    data = request.json
+    response = requests.post(f"{TRAINER_SERVICE_URL}/trainers", json=data)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/trainers', methods=['GET'])
+def get_trainers():
+    response = requests.get(f"{TRAINER_SERVICE_URL}/trainers")
     return jsonify(response.json()), response.status_code
 
 if __name__ == '__main__':
